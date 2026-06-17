6b4db78fb72b932b75df24f3d0a4788aa8529d1d
ahmadUffi <ahmaduffi45@gmail.com>
2026-06-17 13:08:33 +0700

    update:NEXT_PUBLIC_SITE_URL

--- app/forgot-password/actions.ts
+++ app/forgot-password/actions.ts
@@ -10,10 +10,12 @@ const forgotPasswordSchema = z.object({
 });
 
 async function getOrigin() {
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  }
   const headerStore = await headers();
   const host = headerStore.get("host") ?? "localhost:3000";
   const protocol = headerStore.get("x-forwarded-proto") ?? "http";
-
   return `${protocol}://${host}`;
 }
 
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -24,10 +24,12 @@ async function getCredentials(formData: FormData) {
 }
 
 async function getOrigin() {
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  }
   const headerStore = await headers();
   const host = headerStore.get("host") ?? "localhost:3000";
   const protocol = headerStore.get("x-forwarded-proto") ?? "http";
-
   return `${protocol}://${host}`;
 }
 
